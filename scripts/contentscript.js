d95bc3a5029dec68f5d5ead72dc71c368d129e99
tivonn <tivon.x@qq.com>
2020-03-02 21:59:03 +0800

    feat: capture dom screenshot

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -3,25 +3,39 @@
   await import(chrome.runtime.getURL("scripts/utils/fileSaver.min.js"))
 })()
 
-const screenshot = async () => {
-  const dom = document.body
-  const width = dom.offsetWidth
-  const height = dom.offsetHeight
+const screenshot = async (dom) => {
+  let container
+  try {
+    container = document.querySelector(dom || 'body')
+  } catch {
+    console.log('error from querySelector')
+  }
+  if (!container) {
+    chrome.runtime.sendMessage({
+      from: 'contentscript',
+      action: 'setNoDomVisible',
+      isVisible: true
+    })
+    return
+  }
+  const width = container.scrollWidth
+  const height = container.scrollHeight
   const factor = 0.6
   const canvas = await window.html2canvas(
-    dom,
+    container,
     {
       windowWidth: width * factor,
       windowHeight: height * factor,
-      scale: 1.2
+      scale: 1.2,
+      useCORS: true
     }
   )
   const fileName = `${ document.title ? `${document.title} - ` : ''}${Date.now()}`
   canvas.toBlob(blob => window.saveAs(blob, fileName))
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(request => {
   if (request.from === 'popup' && request.action === 'screenshot') {
-    screenshot()
+    screenshot(request.dom)
   }
 })
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,31 @@
-let screenshot = document.getElementById('screenshot')
+let domInput = document.querySelector('.dom-input')
+let noDom = document.querySelector('.no-dom')
+let screenshot = document.querySelector('.screenshot')
+
 screenshot.onclick = () => {
+  setVisible(noDom, false)
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.sendMessage(
       tabs[0].id,
       {
         from: 'popup',
-        action: 'screenshot'
+        action: 'screenshot',
+        dom: domInput.value
       }
     )
   })
 }
+
+const setVisible = (dom, isVisible) => {
+  if (isVisible) {
+    dom.style.display = 'inline-block'
+  } else {
+    dom.style.display = 'none'
+  }
+}
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request.from === 'contentscript' && request.action === 'setNoDomVisible') {
+    setVisible(noDom, request.isVisible)
+  }
+})
